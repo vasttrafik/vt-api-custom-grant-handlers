44880b251d4766c16d0a46675925e8b8cd9fa969
Lars Andersson <lars.andersson@vasttrafik.se>
2016-05-16 08:22:35 +0200

    Made eviction algorithm go to sleep for the duration of the run.

--- src/org/vasttrafik/wso2/carbon/caching/impl/eviction/NoneEvictionAlgorithm.java
+++ src/org/vasttrafik/wso2/carbon/caching/impl/eviction/NoneEvictionAlgorithm.java
@@ -3,17 +3,39 @@ package org.vasttrafik.wso2.carbon.caching.impl.eviction;
 import java.util.TreeSet;
 
 import org.wso2.carbon.caching.impl.eviction.EvictionAlgorithm;
+import org.wso2.carbon.caching.impl.CacheEntry;
+import org.wso2.carbon.caching.impl.CacheImpl;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.wso2.carbon.caching.impl.CacheEntry;
 
 public class NoneEvictionAlgorithm implements EvictionAlgorithm
 {
   private static final Log log = LogFactory.getLog(NoneEvictionAlgorithm.class);
+  private CacheImpl cache;
   
+  public NoneEvictionAlgorithm() {
+  }
+  
+  public void setCache(CacheImpl cache) {
+	  this.cache = cache;
+  }
+  
+  // TO-DO: Check if number of entries exceed capacity, if so, try to find something to evict instead of going to sleep...
   public CacheEntry getEntryForEviction(TreeSet<CacheEntry> evictionList)
   {
-    log.debug("Trying to find value to evict");
+	if (cache != null) {
+	  final String message = "Cache " + cache.getName() + " with " + cache.getAll().size() + " entries going to sleep...";
+      log.info(message);
+	}
+    
+    try {
+    	Thread.currentThread().sleep(60000L);
+    }
+    catch (InterruptedException i) {
+      log.info("Interrupted");	
+    }
+    catch (Exception e) {}
     return null;
   }
 }
